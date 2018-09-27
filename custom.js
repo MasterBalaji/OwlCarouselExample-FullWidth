aa09de05abe4cafadaf0212ca3dbaa1c98d07849
Balaji Donthi <masterbalaji@gmail.com>
2018-09-27 13:45:13 +0530

    Owl carousel single slide

---
+++ custom.js
@@ -0,0 +1,8 @@
+$('.owl-carousel').owlCarousel({
+    loop: true, //Loop the images
+    nav: true, //Display Next and Prev Buttons
+    items: 1, //No of images to display at a time
+    autoplay: true, //Auto play slides on load
+    autoplayTimeout: 5000, //No of seconds for each slide
+    autoplayHoverPause: false, //Stop slide show on mouse hover
+});
